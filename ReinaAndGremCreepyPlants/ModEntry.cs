db1022c3ce555f2737178ea4d757ae931cc0cf2a
reina <ReinaSHSL@users.noreply.github.com>
2019-06-13 14:08:31 -0700

    ModEntry

---
+++ ReinaAndGremCreepyPlants/ModEntry.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewModdingAPI.Utilities;
+using StardewValley;
+
+namespace ReinaAndGremCreepyPlants
+{
+    public class ModEntry : Mod
+    {
+        public override void Entry(IModHelper helper)
+        {
+
+        }
+    }
+}
